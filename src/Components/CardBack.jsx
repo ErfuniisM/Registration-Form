e7473231e107daa5a2c8c490ad566bad0b55ddc2
ErfunisM <erfuniism@gmail.com>
2023-04-25 13:42:35 +0330

    Created card back and front

---
+++ src/Components/CardBack.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function CardBack() {
+  return <h1>Just For Test</h1>
+}
+export default CardBack;
---
+++ src/Components/CardFront.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function CardFront() {
+  return <h1>Just For Test</h1>
+}
+export default CardFront;
