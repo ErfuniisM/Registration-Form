2a912b636ccac94b59a9954d2eed98cc53acf8d3
ErfunisM <erfuniism@gmail.com>
2023-04-27 10:51:04 +0330

    Created Registration Form Style

--- src/Components/CardFront.jsx
+++ src/Components/CardFront.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import { Box } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 function CardFront() {
-  return <Box></Box>
+  return (
+    <Box className={"card_front"}>
+      <Box className={"form-style"}>
+        <TextField label="Username" />
+        <TextField label="Email" />
+        <TextField label="Password" />
+        <TextField label="Re-Password" />
+        <Button variant="contained" color="secondary">
+          SUBMIT
+        </Button>
+      </Box>
+    </Box>
+  );
 }
 export default CardFront;
