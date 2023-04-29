d0db26a6ee733bd03e44c2f25944adff15464793
ErfunisM <erfuniism@gmail.com>
2023-04-29 15:59:21 +0330

    Create CardBack.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import "./App.css";
 import { Box } from "@mui/material";
 import CardFront from "./Components/CardFront";
+import CardBack from "./Components/CardBack";
 function App() {
-  return <Box>
-    <CardFront/>
-  </Box>
+  return (
+    <Box>
+      <CardFront />
+      <CardBack />
+    </Box>
+  );
 }
 export default App;
--- src/Components/CardBack.jsx
+++ src/Components/CardBack.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { Box } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 function CardBack() {
-  return <h1>Just For Test</h1>
+  return (
+    <Box className={"card_front"}>
+      <Typography
+        sx={{
+          color: "cadetblue",
+          fontWeight: "900",
+          fontFamily: "Dosis, sans-serif !important",
+          fontSize: "x-large",
+        }}
+      >
+        Join to My World!
+      </Typography>
+      <FormControl className={"form-style"}>
+        <TextField label="Username" />
+        <TextField label="Password" />
+        <Button variant="contained" color="secondary">
+          Login
+        </Button>
+      </FormControl>
+      <Link href="#" underline="none" sx={{ color: "cadetblue" }}>
+        Create Account
+      </Link>
+    </Box>
+  );
 }
 export default CardBack;
